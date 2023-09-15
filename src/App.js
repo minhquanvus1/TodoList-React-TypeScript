33e87b76ce0ae55eeeb6a79092f7b0b16ab9a0cb
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-15 12:44:37 +0700

    add onClick event listener for button

--- src/App.js
+++ src/App.js
@@ -12,12 +12,27 @@ function App() {
       rowAssigned: "User 1",
     },
   ];
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newToDo = {
+        rowNumber: todos[todos.length - 1].rowNumber + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User 3",
+      };
+      todos.push(newToDo);
+      console.log("Todo List: ", todos);
+    }
+  };
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your ToDos</div>
         <div className="card-body">
           <TodoTable todos={todos}></TodoTable>
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new Todo
+          </button>
         </div>
       </div>
     </div>
